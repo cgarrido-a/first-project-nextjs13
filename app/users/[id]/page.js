6961ddc48c4a0e0748acec4c6d51352a1e5c3ebe
Consuelo Garrido Abedrapo <consuelo@desafiolatam.com>
2023-07-18 11:53:47 -0400

    add bootstrap style to user page

--- app/users/[id]/page.js
+++ app/users/[id]/page.js
@@ -1,25 +1,22 @@
 async function getUser(id) {
-    // console.log(id)
     const res = await fetch(`https://reqres.in/api/users/${id}`)
     const data = await res.json()
     return data.data
-    // return{
-    //     test: "hello world"
-    // }
-
 }
 
 async function UsersPage({params}) {
     const user = await getUser(params.id);
-    // console.log(user);
     return (
-        <div>
-            <h1>User Details</h1>
-            <div>
-                <img src={user.avatar} alt="user avatar"/>
-                <div>
-                    <h3>{user.first_name} {user.last_name}</h3>
-                    <p>{user.email}</p>
+        <div className="row">
+            <div className="col-md-6 offset-md-3">
+                <div className="card">
+                    <div className="card-header text-center">
+                        <img src={user.avatar} alt="user avatar"/>
+                    </div>
+                    <div className="card-body text-center">
+                        <h3>{user.first_name} {user.last_name}</h3>
+                        <p>{user.email}</p>
+                    </div>
                 </div>
             </div>
         </div>
